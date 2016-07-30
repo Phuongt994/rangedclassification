2e991937436f2bdb25c04850bd7a40852aafa3b4
Phuongt994 <phuongt994@gmail.com>
2016-07-30 15:22:51 +0100

    30.07.16 - Archived Analyser

--- src/Tools/Analyser.java
+++ src/Tools/Analyser.java
@@ -12,13 +12,12 @@ public class Analyser {
     // LinkedHashMap to preserve order of insertion
     private LinkedHashMap<Float[], LinkedList<LinkedList>> attrSorted;
     private LinkedList attrVal;
-    private LinkedHashMap<Integer, Float[]> LR;
+    private LinkedHashMap<Integer, LinkedList<Float[]>> LR;
 
     public Analyser(LinkedList<LinkedList> tuple, HashMap classMap) {
         this.tuple = new LinkedList<>();
         this.tuple = tuple;
         this.classMap = new HashMap<>(classMap);
-
         maxMin(this.classMap);
     }
 
@@ -26,34 +25,29 @@ public class Analyser {
 
         map.keySet().forEach(k -> {
             String classTag = k;
-            // keep track of attribute quantity
-            // to be put in report()
             int attrCount = 0;
 
-            // sorted attr list for EACH class
+            System.out.println("------------------------");
+            System.out.println("For class " + classTag);
+
             attrSorted = new LinkedHashMap<>();
 
             // for each attribute
-            // starts at 1 end at size - 1
-            for (int i = 1; i < map.get(k).get(0).size() - 1; i++) {
+            for (int i = 1; i < map.get(k).get(0).size() -1; i++) {
                 int order = i;
                 attrVal = new LinkedList<Float>();
                 map.get(k).stream().forEach(t -> {
                     attrVal.add(t.get(order));
                 });
-                System.out.println("------------------------");
-                System.out.println("For class " + k + " : " + "List of attr number " + i + " : " + attrVal);
+                System.out.println("List of attr number " + i + " : " + attrVal);
                 attrCount++;
 
-                // use sort() to get max and min value of range
                 Collections.sort(attrVal);
                 Float[] attrRange = new Float[2];
-                attrRange[0] = (Float) attrVal.get(0);
-                attrRange[1] = (Float) attrVal.get(attrVal.size() - 1);
+                attrRange[0] = (Float) attrVal.getFirst();
+                attrRange[1] = (Float) attrVal.getLast();
                 System.out.println("Range of attr number " + i + " : " + attrRange[0] + " " + attrRange[1]);
 
-                // find tuples that are in range
-                // from general tuple list not hashmap tuple
                 LinkedList tupleAList = new LinkedList<LinkedList>();
                 tuple.stream().forEach(t -> {
                     if (attrRange[0] <= (Float) t.get(order) && (Float) t.get(order) <= attrRange[1]) {
@@ -62,46 +56,36 @@ public class Analyser {
                 });
 
                 System.out.println("TupleAList in hashMap: " + tupleAList);
-
-                // append into hashmap!
                 attrSorted.put(attrRange, tupleAList);
+
             }
             System.out.println("Attr count : " + attrCount);
-            // create long range holder
-            LinkedHashMap LR = new LinkedHashMap<>();
+            LinkedHashMap LR = new LinkedHashMap<Integer, LinkedList<Float[]>>();
             biConvert(classTag, attrSorted, LR);
+
             System.out.println("Done 1 class - sending to generator\n");
-            // LR as param
-            // CALL GENERATOR INSTANCE HERE
+            System.out.println("All entryset: " + LR.entrySet());
+            new Generator(classTag, LR);
         });
     }
 
-    private void biConvert(String classTag, LinkedHashMap<Float[], LinkedList<LinkedList>> attrMap, LinkedHashMap<Integer, Float[]> LR) {
-        System.out.println("---------------------------------");
-        System.out.println("For class " + classTag);
-        // 1 or -1 transform of class in each tuple
+    private void biConvert(String classTag, LinkedHashMap<Float[], LinkedList<LinkedList>> attrMap, LinkedHashMap<Integer, LinkedList<Float[]>> LR) {
+        System.out.println("\nbiConvert() for class " + classTag);
 
-        // create a clone hashmap to avoid editing the original version
+        // create a clone hashmap
         LinkedHashMap<Float[], LinkedList<LinkedList>> tempMap = new LinkedHashMap<>(attrMap);
 
-
-        List keys = new ArrayList(tempMap.keySet());
-        for (int i = 0; i < keys.size(); i++) {
-            int attrNo = i;
-
-            // to control which attribute number it is (using i)
-            Object keyObj = keys.get(i);
-
-
-            Comparator<LinkedList> comp = (a, b)-> ((Float) a.get(attrNo+1)).compareTo((Float) b.get(attrNo+1));
-            tempMap.get(keyObj).sort(comp);
-            tempMap.put((Float[]) keyObj, tempMap.get(keyObj));
-
-            System.out.println("Sorted map by attr no. : " + (attrNo+1) + "||| "  + tempMap.get(keyObj));
+        int order = 0;
+        for (Object key : tempMap.keySet()) {
+            ++order;
+            int attrNo = order;
+            Comparator<LinkedList> comp = (a, b)-> ((Float) a.get(attrNo)).compareTo((Float) b.get(attrNo));
+            tempMap.get(key).sort(comp);
+            System.out.println("Sorted map by attr no. : " + (attrNo) + " || "  + tempMap.get(key));
 
             // create a temp binary list
             LinkedList biTuple = new LinkedList<Integer>();
-            tempMap.get(keyObj).stream().forEach(tuple -> {
+            tempMap.get(key).stream().forEach(tuple -> {
                 // append 1 and -1 into an array for max sum solution
                 if (tuple.get(tuple.size() - 1).equals(classTag)) {
                     biTuple.add(1);
@@ -111,7 +95,7 @@ public class Analyser {
             });
 
             System.out.println("Converted binary list: " + biTuple);
-            maxSum(keyObj, tempMap, attrNo, biTuple, LR);
+            maxSum(key, tempMap, attrNo, biTuple, LR);
         };
     }
 
@@ -120,86 +104,93 @@ public class Analyser {
      * Original method from Kadane's algorithm
      * @param biList a list of 1 or -1 for max sum calculation
      */
-    private void maxSum(Object key, LinkedHashMap<Float[], LinkedList<LinkedList>> map, int attr, LinkedList<Integer> biList, LinkedHashMap<Integer, Float[]> LR) {
+    private void maxSum(Object key, LinkedHashMap<Float[], LinkedList<LinkedList>> map, int attr, LinkedList<Integer> biList, LinkedHashMap<Integer, LinkedList<Float[]>> LR) {
         System.out.println("Size of biList: " + biList.size());
-        int curMax = -1;
+        int curMax = 0;
         int[] curPos = {0, 0};
+        int startInd = 0;
+        boolean prevPos = false;
         LinkedList<int[]> allPos = new LinkedList<>();
 
         for (int i = 0; i < biList.size(); i++) {
             curMax += biList.get(i);
-            if (curMax < -1) {
+            if (curMax < 0) {
                 //reset
-                if (curPos[0] > curPos[1]) {
-                    continue;
+                if (prevPos == true) {
+                    allPos.add(curPos.clone());
                 }
-                allPos.add(curPos.clone());
-                curPos[0] = i;
+                startInd = i+1;
+                curMax = 0;
+                prevPos = false;
+
+            //} else if (i == biList.size() -1) {
+            //    allPos.add(curPos);
+            //    break;
             } else {
+                curPos[0] = startInd;
                 curPos[1] = i;
+                prevPos = true;
             }
         }
-        // allPos = allPos.stream().distinct().collect(Collectors.toCollection(LinkedList<int[]>::new));
+
+        //allPos = allPos.stream().distinct().collect(Collectors.toCollection(LinkedList<int[]>::new));
 
         System.out.println("All positions: ");
         allPos.stream().forEach(p -> {
             System.out.println(Arrays.toString(p));
         });
 
-        // recursive loop needed? DOES NOT WORK YET -- push?
-//        int count = 0;
-//        int start, end;
-//        for (int k = 0; k < temp.size(); k++) {
-//            if (temp.indexOf(k) < temp.size() -1) {
-//                if ((int) temp.get(k) == 1) {
-//                    start = temp.indexOf(k);
-//                    if ((int) temp.get(k + 1) == 1) {
-//                        continue;
-//                    } else {
-//                        if ((int) temp.get(k + 1) == -1) {
-//                            count++;
-//                            if (count == 2) {
-//                                end = temp.indexOf(k + 1);
-//                                System.out.println("start and end ind subList :" + start + " " + end);
-//                                break;
-//                            }
-//                        }
-//                    }
-//                }
-//            } else {
-//                break;
-//            }
-//        }
-
-
-        //addCRange(key, map, attr, allPos, biList, LR);
-    }
-
-    private void addCRange(Object key, LinkedHashMap<Float[], LinkedList<LinkedList>> map, int attr, int[] indRange, LinkedList<Integer> biList, LinkedHashMap<Integer, Float[]> LR) {
-        // after maxsum called
-        Float[] cRange = new Float[2];
-        cRange[0] = (Float) map.get(key).get(indRange[0]).get(attr+1);
-        cRange[1] = (Float) map.get(key).get(indRange[1]).get(attr+1);
-
-        System.out.println("Range after maxsum: " + cRange[0] + " " + cRange[1]);
-        minThresh(biList, cRange, indRange, LR, attr);
+        minThresh(key, map, attr, allPos, biList, LR);
     }
 
     /***
      * Check for support & confidence (then density)
      *
      */
-    private void minThresh(LinkedList<Integer> biList, Float[] cRange, int[] indRange, LinkedHashMap<Integer, Float[]> LR, int attr) {
-        float support =(float) (indRange[1] - indRange[0]) / biList.size();
-        System.out.println("Support: " + support);
+    private void minThresh(Object key, LinkedHashMap<Float[], LinkedList<LinkedList>> map, int attr, LinkedList<int[]> allRange, LinkedList<Integer> biList, LinkedHashMap<Integer, LinkedList<Float[]>> LR) {
+        LinkedList list = new LinkedList<Integer>();
 
-        System.out.println("Confidence: " + 1 + " ?" + "\n-------------------");
+        LinkedList cRange = new LinkedList<Float[]>();
 
-        // suppose support > 60%
-        // NEED CHECK FOR DENSITY
-        LR.put(attr, cRange);
+        for (int[] range : allRange) {
+            // eliminate 0,0
+            float support = (float) (range[1] - range[0]) / biList.size();
+
+            // for confidence
+            for (int i = 0; i < range[1] - range[0]; i++) {
+                list.add(biList.get(i));
+            }
+
+            int pos = 0;
+            int neg = 0;
+            for (int j = 0; j < list.size(); j++) {
+                if ((int) list.get(j) == 1) {
+                    pos++;
+                }
+            }
+            float confidence = (float) pos / list.size();
+
+            System.out.println("Support: " + support);
+            System.out.println("Confidence: " + confidence);
+
+            if (support <  0.1) {
+                System.out.println("Rejected");
+            } else {
+                Float[] aRange = new Float[2];
+                aRange[0] = (Float) map.get(key).get(range[0]).get(attr);
+                aRange[1] = (Float) map.get(key).get(range[1]).get(attr);
+                System.out.println("Converted aRange: " + Arrays.asList(aRange));
+                cRange.add(aRange);
+            }
+        }
+        System.out.println("cRange distinct()");
+        cRange = (LinkedList<Float[]>) cRange.stream().distinct().collect(Collectors.toCollection(LinkedList<Float[]>::new));
+        if (cRange.isEmpty()) {
+            System.out.println("No ranges found.");
+        } else {
+            LR.put(attr, cRange);
+            System.out.println("LR updated to " + LR.entrySet());
+        }
     }
-
-
 
 }
--- src/Tools/Generator.java
+++ src/Tools/Generator.java
@@ -1,19 +1,33 @@
 package Tools;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 /**
  * Created by Phuongt994 on 10/07/2016.
  */
 public class Generator {
-    private LinkedHashMap<Integer, Float[]> LR;
-    public Generator(LinkedHashMap<Integer, Float[]> LR) {
+    private LinkedHashMap<Integer, LinkedList<Float[]>> LR;
+    private String classTag;
+    public Generator(String classTag, LinkedHashMap<Integer, LinkedList<Float[]>> LR) {
         this.LR = LR;
+        this.classTag = classTag;
+        System.out.println("Generator started for " + classTag);
+        System.out.println("LR entries: ");
+        for (Object key : LR.keySet()) {
+            System.out.println((Integer) key);
+            for (Float[] fl : LR.get(key)) {
+                System.out.println(Arrays.asList(fl));
+            };
+        }
         aPriori(this.LR);
-
     }
 
-    private void aPriori(LinkedHashMap<Integer, Float[]> LR) {
-
+    private void aPriori(LinkedHashMap<Integer, LinkedList<Float[]>> LR) {
+        for (Float[] range : LR.entrySet())
+                }
+            }
+        }
     }
 }
