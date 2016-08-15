2cba4c0eff125601f1d45490de5ce3a4652fc0bf
c1571743 <c1571743@mu.cs.cf.ac.uk>
2016-08-15 16:39:07 +0100

    15.08.16 - analyser fixed, generator started/undone maclab

--- src/Tools/Analyser.java
+++ src/Tools/Analyser.java
@@ -27,16 +27,6 @@ public class Analyser {
         maxMin(this.allClassMap);
     }
     
-    /*** 
-     * @param allClassMap class map 
-     * c one class
-     * aC attribute count
-     * aN attribute number
-     * allAttributeMap attribute-sorted map 
-     * attributeValue attribute-sorted values
-     * aR one attribute range
-     * aRallTuple attribute-range sorted tuples
-     */
     private void maxMin(HashMap<String, LinkedList<LinkedList>> allClassMap) {
         allClassMap.keySet().forEach(k -> {
             String classTag = k;
@@ -54,7 +44,7 @@ public class Analyser {
                 allClassMap.get(k).stream().forEach(t -> {
                     attributeValue.add((Float) t.get(attributeNumber));
                 });
-                System.out.println("List of attr number " + attributeNumber + " : " + attributeValue);
+                // System.out.println("List of attr number " + attributeNumber + " : " + attributeValue);
                 attributeCount++;
 
                 Collections.sort(attributeValue);
@@ -70,32 +60,27 @@ public class Analyser {
                     }
                 });
 
-                System.out.println("Attribute-range sorted tuples for attr no " + i + " is : " + attributeRangedTuple);
+                // System.out.println("Attribute-range sorted tuples for attr no " + i + " is : " + attributeRangedTuple);
                 // append to attribute map aM
                 allAttributeMap.put(attributeRange, attributeRangedTuple);
 
             }
             System.out.println("Attr count : " + attributeCount);
-            System.out.println("Attr map values: " + allAttributeMap.entrySet());
+            // System.out.println("Attr map values: " + allAttributeMap.entrySet());
             
             /***
              * LR? CR?
              */
             LinkedHashMap LR = new LinkedHashMap<Integer, LinkedList<Float[]>>();
-            LinkedList CR = new LinkedList<LinkedList<Float>>();
-            binaryConvert(classTag, allAttributeMap, CR);
+            // LinkedList CR = new LinkedList<LinkedList<Float>>();
+            binaryConvert(classTag, allAttributeMap, LR);
 
             System.out.println("Done 1 class - sending to generator\n");
-//            System.out.println("All entries ");
-//            for (int i = 0; i < CR.size(); i++) {
-//                System.out.println(((LinkedList<Float>) CR.get(i)).toString());
-//            }
-
-            //new Generator(classTag, CR);
+            new Generator(classTag, LR);
         });
     }
 
-    private void binaryConvert(String classTag, LinkedHashMap<Float[], LinkedList<LinkedList>> allAttributeMap, LinkedList<LinkedList<Float>> CR) {
+    private void binaryConvert(String classTag, LinkedHashMap<Float[], LinkedList<LinkedList>> allAttributeMap, LinkedHashMap<Integer, LinkedList<Float[]>> LR) {
         System.out.println("\nbiConvert() for class " + classTag);
 
         // create a clone hashmap
@@ -107,7 +92,7 @@ public class Analyser {
             int attributeNumber = order;
             Comparator<LinkedList> comp = (a, b)-> ((Float) a.get(attributeNumber)).compareTo((Float) b.get(attributeNumber));
             tempAttributeMap.get(key).sort(comp);
-            System.out.println("Sorted map by attr no. : " + (attributeNumber) + " || "  + tempAttributeMap.get(key));
+            // System.out.println("Sorted map by attr no. : " + (attributeNumber) + " || "  + tempAttributeMap.get(key));
 
             // create a temp binary list
             LinkedList<Integer> binaryList = new LinkedList<>();
@@ -120,18 +105,17 @@ public class Analyser {
                 }
             });
 
-            System.out.println("Converted binary list: " + binaryList);
-            maxSum(key, tempAttributeMap, attributeNumber, binaryList, CR);
+            System.out.println("Converted binary list for attribute no. " + attributeNumber + ": " + binaryList);
+            maxSum(key, tempAttributeMap, attributeNumber, binaryList, LR);
         };
     }
 
     /**
-     * Altered max sum with iteration and additional decremental limit (NOT YET APPLIED)
      * Original method from Kadane's algorithm
      * @param biList a list of 1 or -1 for max sum calculation
      */
-    private void maxSum(Object key, LinkedHashMap<Float[], LinkedList<LinkedList>> tempAttributeMap, int attributeNumber, LinkedList<Integer> binaryList, LinkedList<LinkedList<Float>> CR) {
-        System.out.println("Size of biList: " + binaryList.size());
+    private void maxSum(Object key, LinkedHashMap<Float[], LinkedList<LinkedList>> tempAttributeMap, int attributeNumber, LinkedList<Integer> binaryList, LinkedHashMap<Integer, LinkedList<Float[]>> LR) {
+        // System.out.println("Size of biList: " + binaryList.size() + " for attr number " + attributeNumber);
         int currentMax = 0;
         int[] currentPosition = {0, 0};
         boolean prevSumIsPositive = false;
@@ -158,20 +142,24 @@ public class Analyser {
         }
         //allPos = allPos.stream().distinct().collect(Collectors.toCollection(LinkedList<int[]>::new));
 
-        System.out.println("All positions: ");
+        System.out.println("All positions for attribute number " + attributeNumber + " is :");
+        // transform it back to float ranges
+        LinkedList<Float[]> CR = new LinkedList<>();
         allPosition.stream().forEach(p -> {
             System.out.println(Arrays.toString(p));
-        });
-
-        // minThresh(key, tempAttributeMap, attributeNumber, allPosition, binaryList, CR);
+            Float[] aFloatRange = new Float[2];
+            aFloatRange[0] = (Float) tempAttributeMap.get(key).get(p[0]).get(attributeNumber);
+            aFloatRange[1] = (Float) tempAttributeMap.get(key).get(p[1]).get(attributeNumber);
+            CR.add(aFloatRange);
+        }); 
+        LR.put(attributeNumber, CR);
     }
     /***
      * Check for threshold within maxsum
      */
-    
     private boolean checkThresh(int[] currentPosition, LinkedList<Integer> binaryList) {
     	// for support
-    	float support = (float) currentPosition[1] - currentPosition[0] / binaryList.size();
+    	float support = (float) (currentPosition[1] - currentPosition[0]) / binaryList.size();
     	
     	// for confidence
     	LinkedList tempList = new LinkedList<Integer>();
@@ -187,13 +175,13 @@ public class Analyser {
         }
         float confidence = (float) positiveCount / tempList.size();
         
-        System.out.println("Support: " + support);
-        System.out.println("Confidence: " + confidence);
-        
         // conditions
         // support & confidence current accepted as positive (no rejects)
         if (support > 0.0) { 
-        	if (confidence > 0.0) {
+        	if (confidence > 0.3) {
+        		System.out.println("Range " + Arrays.toString(currentPosition) + " is accepted");
+        		System.out.println("Support: " + support);
+                System.out.println("Confidence: " + confidence);
         		return true;
         	} else {
         		return false;
@@ -202,50 +190,5 @@ public class Analyser {
         	return false;
         }
     }
-    /***
-     * Check for support & confidence (then density)
-     *
-     */
-    private void minThresh(Object key, LinkedHashMap<Float[], LinkedList<LinkedList>> map, int attr, LinkedList<int[]> allRange, LinkedList<Integer> biList, LinkedList<LinkedList<Float>> CR) {
-        LinkedList list = new LinkedList<Integer>();
-
-        for (int[] range : allRange) {
-            // for support
-            float support = (float) (range[1] - range[0]) / biList.size();
-
-            // for confidence
-            for (int i = 0; i < range[1] - range[0]; i++) {
-                list.add(biList.get(i));
-            }
-
-            int pos = 0;
-            for (int j = 0; j < list.size(); j++) {
-                if ((int) list.get(j) == 1) {
-                    pos++;
-                }
-            }
-            float confidence = (float) pos / list.size();
-
-            System.out.println("Support: " + support);
-            System.out.println("Confidence: " + confidence);
-
-            if (support <  0.0) { // NO REJECTION
-                System.out.println("Rejected");
-            } else {
-                LinkedList<Float> aRange = new LinkedList<>();
-                aRange.add((Float) map.get(key).get(range[0]).get(attr));
-                aRange.add((Float) map.get(key).get(range[1]).get(attr));
-                aRange.addLast((float) attr);
-                System.out.println("Converted aRange: " + Arrays.asList(aRange));
-                CR.add(aRange);
-            }
-        }
-
-        CR = (LinkedList<LinkedList<Float>>) CR.stream().distinct().collect(Collectors.toCollection(LinkedList<LinkedList<Float>>::new));
-        if (CR.isEmpty()) {
-            System.out.println("No ranges found.");
-        } else {
-            System.out.println("cRange for attr no " + attr + " is ready");
-        }
-    }
+    
 }
--- src/Tools/Generator.java
+++ src/Tools/Generator.java
@@ -1,5 +1,6 @@
 package Tools;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -8,32 +9,68 @@ import java.util.LinkedList;
  * Created by Phuongt994 on 10/07/2016.
  */
 public class Generator {
-    private LinkedList<LinkedList<Float>> CR;
+    private LinkedHashMap<Integer, LinkedList<Float[]>> LR;
     private String classTag;
-    public Generator(String classTag, LinkedList<LinkedList<Float>> CR) {
-        this.CR = CR;
+    public Generator(String classTag, LinkedHashMap<Integer, LinkedList<Float[]>> LR) {
+        this.LR = LR;
         this.classTag = classTag;
-        System.out.println("Generator started for " + classTag);
-        //System.out.println("CR entries");
-        aPriori(this.CR);
+        System.out.println("Generator started for " + this.classTag);
+        for (Object key : LR.keySet()) {
+        	System.out.println("For attribute number " + (int) key + " Ranges are: ");
+        	for (Float[] aFloatRange : LR.get(key)) {
+        		System.out.println(Arrays.toString(aFloatRange));
+        	}
+        }
+        aPriori(this.LR);
     }
 
-    private void aPriori(LinkedList<LinkedList<Float>> CR) {
-        for (int i = 0; i < CR.size(); i++) {
-            LinkedList CRG = new LinkedList<LinkedList<LinkedList<Float>>>();
-            LinkedList<Float> target = CR.pop();
-            for (int j = 0; j < CR.size(); j++) {
-                LinkedList temp = new LinkedList<LinkedList<Float>>();
-                if (Math.round(target.getLast()) != Math.round(CR.get(j).getLast())) {
-                    temp.add(target);
-                    temp.add(CR.get(j));
-                    CRG.add(temp);
-                }
-            }
-            System.out.println("CR entries after REGEN: ");
-            for (int k = 0; k < CRG.size(); k++) {
-                System.out.println((CRG.get(k)).toString());
-            }
+    private void aPriori(LinkedHashMap<Integer, LinkedList<Float[]>> LR) {
+    	System.out.println("Apriori started");
+        LinkedHashMap<int[], LinkedList<LinkedList<Float[]>>> generatedCR = new LinkedHashMap<>();
+        LinkedHashMap<Integer, LinkedList<Float[]>> tempLR = new LinkedHashMap<>(LR);
+        
+        for (Object key : new ArrayList<Object>(tempLR.keySet())) {
+        	Object poppedKey = key;
+        	// get combined key (integer array)
+        	// set combined key 0 = popped key
+        	int[] aCombinedKey = new int[2];
+        	aCombinedKey[0] = (int) poppedKey;
+    		// get combined range (linked list of linked list of float)
+    		LinkedList<LinkedList<Float[]>> combinedRange = new LinkedList<>();
+    		
+    		// get popped key's ranges before delete
+    		LinkedList<Float[]> poppedKeyRange = new LinkedList<>(tempLR.get(poppedKey));
+    		tempLR.keySet().remove(poppedKey);
+    		
+    		// for each remaining key range
+    		for (Object remainingKey : tempLR.keySet()) {
+    			// set combined key 1 = remaining key
+				aCombinedKey[1] = (int) remainingKey;
+				
+				// for each popped key range
+    			 for (Float[] aPoppedKeyRange : poppedKeyRange) {
+    				 
+    				// for each range in remaining key
+    				for (Float[] aRemainingKeyRange : tempLR.get(remainingKey)) {
+    					// get a 'combined' range
+    					LinkedList<Float[]> aCombinedRange = new LinkedList<>();
+    					aCombinedRange.add(aPoppedKeyRange);
+    					aCombinedRange.add(aRemainingKeyRange);
+    					// add combined range to big combined range list
+    					combinedRange.add(aCombinedRange);
+    				} 			
+    			}
+    		}
+        	// put key and range in generated CR 
+        	generatedCR.put(aCombinedKey, combinedRange);
+        }
+        
+        System.out.println("Generated done");
+        for (Object key : generatedCR.keySet()) {
+        	System.out.println("New attribute combination : " + Arrays.toString((int[]) key));
+        	for (LinkedList<Float[]> val : generatedCR.get(key)) {
+        		System.out.println(">> Ranges are : " + val);
+        	}
         }
     }
 }
