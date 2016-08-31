8bdfe7e75c55fcaac0705a13cd8ae2fe3513cb93
c1571743 <c1571743@carbon>
2016-08-31 21:40:56 +0100

    31.08.16 - Finished code with runtime check

--- src/Starter.java
+++ src/Starter.java
@@ -4,8 +4,32 @@ import Tools.Scanners;
  * Created by phuongt994 on 17/06/2016.
  */
 public class Starter {
+	 
     public static void main(String[] args) {
-        new Scanners();
-    }
+    	// get start time
+    	final long startTime = System.nanoTime();
+    	
+    	new Scanners();
+    	
+    	// get end time
+    	// calculate duration
+    	final long duration = System.nanoTime() - startTime;
+    	final long durationInMillisecond = duration / 1000000;
+    	final long durationInSecond = duration / 1000000000;
+    	System.out.println("Execution time : " + durationInMillisecond + " milliseconds.");
+    	System.out.println("Execution time : " + durationInSecond + " seconds.");
+    	
+    	// Get the Java runtime
+        Runtime runtime = Runtime.getRuntime();
+        // Run the garbage collector
+        runtime.gc();
+        // Calculate the used memory
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used memory is " + memory + " in bytes");
+        long kiloByte = 1024L;
+        long memoryInKB = memory / kiloByte;
+        System.out.println("Used memory is " + memoryInKB + "in kilobytes");
+      }
+    
 }
 
